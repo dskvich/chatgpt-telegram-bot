8d260b6c77d37f42fac7c467a6b16a438c8c133f
Dima <sushkevichd@gmail.com>
2023-05-17 22:09:04 +0300

    added allowed user ids

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module github.com/antelman107/chatgpt-telegram-bot
+module chatgpt-telegram-bot
 
 go 1.20
 
 require (
 	github.com/franciscoescher/goopenai v0.4.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc
 )
 
 require github.com/google/go-querystring v1.1.0 // indirect
--- main.go
+++ main.go
@@ -4,16 +4,34 @@ import (
 	"context"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/franciscoescher/goopenai"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"golang.org/x/exp/slices"
 )
 
+var allowedIds  []int64
+
 func main() {
+	for _, s := range strings.Split(os.Getenv("TELEGRAM_ALLOWED_IDS"), ",") {
+		id, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			log.Printf("Error converting string '%s' to int64: %v\n", s, err)
+			continue
+		}
+		allowedIds = append(allowedIds, id)
+	}
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -24,6 +42,9 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
+			if !isAllowed(update.Message.From.ID) {
+				continue
+			}
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			r := goopenai.CreateCompletionsRequest{
@@ -57,4 +78,8 @@ func main() {
 			}
 		}
 	}
+}
+
+func isAllowed(id int64) bool {
+	return slices.Contains(allowedIds, id)
 }
