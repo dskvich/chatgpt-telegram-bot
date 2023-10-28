6997259da1c5ef289936a0ae53e8d1661e39ad2b
Dima <sushkevichd@gmail.com>
2023-10-28 18:41:54 +0300

    fixed draw callback and the 'more' button

--- pkg/command/handler/draw_callback.go
+++ pkg/command/handler/draw_callback.go
@@ -32,19 +32,23 @@ func (d *drawCallback) CanHandle(update *tgbotapi.Update) bool {
 }
 
 func (d *drawCallback) Handle(update *tgbotapi.Update) {
-	imgBytes, err := d.provider.GenerateImage(update.CallbackQuery.Message.Text)
+	imgBytes, err := d.provider.GenerateImage(update.CallbackQuery.Message.ReplyToMessage.Text)
 	if err != nil {
 		d.outCh <- &domain.TextMessage{
 			ChatID:           update.CallbackQuery.Message.Chat.ID,
-			ReplyToMessageID: update.CallbackQuery.Message.MessageID,
+			ReplyToMessageID: update.CallbackQuery.Message.ReplyToMessage.MessageID,
 			Content:          fmt.Sprintf("Failed to generate image using Dall-E: %v", err),
 		}
 		return
 	}
 
+	d.outCh <- &domain.CallbackMessage{
+		ID: update.CallbackQuery.ID,
+	}
+
 	d.outCh <- &domain.ImageMessage{
 		ChatID:           update.CallbackQuery.Message.Chat.ID,
-		ReplyToMessageID: update.CallbackQuery.Message.MessageID,
+		ReplyToMessageID: update.CallbackQuery.Message.ReplyToMessage.MessageID,
 		Content:          imgBytes,
 	}
 }
