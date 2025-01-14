5cee7fb426bda1d2b90908fab213e7b012ba903b
dskvich <sushkevichd@gmail.com>
2025-01-14 18:18:01 +0300

    escape HTML special characters in response

--- pkg/render/html.go
+++ pkg/render/html.go
@@ -34,7 +34,7 @@ func (hr *htmlRenderer) BlockCode(out *bytes.Buffer, text []byte, lang string) {
 
 func (hr *htmlRenderer) CodeSpan(out *bytes.Buffer, text []byte) {
 	out.WriteString("<code>")
-	out.WriteString(string(text))
+	out.WriteString(html.EscapeString(string(text)))
 	out.WriteString("</code>")
 }
 
@@ -48,7 +48,7 @@ func (hr *htmlRenderer) HRule(out *bytes.Buffer) {
 
 func (hr *htmlRenderer) BlockQuote(out *bytes.Buffer, text []byte) {
 	out.WriteString("> ")
-	out.Write(text)
+	out.WriteString(html.EscapeString(string(text)))
 	out.WriteByte('\n')
 }
 
@@ -62,7 +62,7 @@ func (hr *htmlRenderer) List(out *bytes.Buffer, text func() bool, flags int) {
 
 func (hr *htmlRenderer) ListItem(out *bytes.Buffer, text []byte, flags int) {
 	out.WriteString("- ")
-	out.Write(text)
+	out.WriteString(html.EscapeString(string(text)))
 	out.WriteByte('\n')
 }
 
